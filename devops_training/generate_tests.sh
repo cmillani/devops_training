252de3b9d9e4c7ad09f8d8d1df369f6e23997366
cmillani <carloseduardomillani@gmail.com>
2018-02-21 14:38:07 -0300

    script to run tests and generate code coverage results

---
+++ devops_training/generate_tests.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+XCPRETTYPATH=`which xcpretty`
+SLATHERPATH=`which slather`
+
+if [ -z "$XCPRETTYPATH" ]
+then
+  echo "Must Have XCPRETTY installed"
+  exit 1
+fi
+
+if [ -z "$SLATHERPATH" ]
+then
+  echo "Must Have SLATH installed"
+  exit 1
+fi
+
+xcodebuild \
+-project devops_training.xcodeproj \
+-scheme devops_training \
+-enableCodeCoverage YES \
+-destination 'platform=iOS Simulator,name=iPhone 8,OS=11.2' \
+test | xcpretty --test --color
+
+slather coverage -x --scheme devops_training ./devops_training.xcodeproj
+
+
