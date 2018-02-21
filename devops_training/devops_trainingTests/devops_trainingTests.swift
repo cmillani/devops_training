99968ac8e06232de405f86cf3672fbbc66790371
cmillani <carloseduardomillani@gmail.com>
2018-02-21 14:05:25 -0300

    Xcode project created

---
+++ devops_training/devops_trainingTests/devops_trainingTests.swift
@@ -0,0 +1,36 @@
+//
+//  devops_trainingTests.swift
+//  devops_trainingTests
+//
+//  Created by Carlos Eduardo Millani on 21/02/18.
+//  Copyright © 2018 caduebahia. All rights reserved.
+//
+
+import XCTest
+@testable import devops_training
+
+class devops_trainingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
