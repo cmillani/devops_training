dc2c4bfcf297c542f38323a866735318bcc563f1
cmillani <carloseduardomillani@gmail.com>
2018-02-21 16:08:54 -0300

    Initial products table view created

---
+++ devops_training/devops_training/ProductTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  ProductTableViewCell.swift
+//  devops_training
+//
+//  Created by Carlos Eduardo Millani on 21/02/18.
+//  Copyright © 2018 caduebahia. All rights reserved.
+//
+
+import UIKit
+
+class ProductTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func setupCell(title: String, price: Double) {
+        productTitle.text = title
+        productPrice.text = String(price)
+    }
+
+}
